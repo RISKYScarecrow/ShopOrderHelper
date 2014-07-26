9655f061534ae866e42313149655d4d2be5f7da0
Matthew Underwood <matthew.b.underwood1995@gmail.com>
2014-07-26 22:55:02 +1000

    Extended the java robot

--- src/com/github/RISKYScarecrow/ShopOrderHelper/ShopOrderHelper.java
+++ src/com/github/RISKYScarecrow/ShopOrderHelper/ShopOrderHelper.java
@@ -1,5 +1,45 @@
 package com.github.RISKYScarecrow.ShopOrderHelper;
 
+
+import java.awt.AWTException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
 public class ShopOrderHelper {
+	
+	public static ExtendedRobot robot;
+	public static Toolkit toolkit;
+	public static Clipboard clipboard;
+	
+	public static void main(String[] args) 
+	{
+
+		try 
+		{
+			robot = new ExtendedRobot();
+			toolkit = Toolkit.getDefaultToolkit();
+			clipboard = toolkit.getSystemClipboard();
+			
+			
+			robot.delay(5000);
+			/*
+			robot.keyUse(KeyEvent.VK_H);
+			robot.keyUse(KeyEvent.VK_I);
+			robot.keyUse(KeyEvent.VK_SPACE);
+			robot.keyUse(KeyEvent.VK_B);
+			robot.keyUse(KeyEvent.VK_U);
+			robot.keyUse(KeyEvent.VK_D);
+			robot.keyUse(KeyEvent.VK_D);
+			robot.keyUse(KeyEvent.VK_Y);
+			*/
+			
+			robot.moveSelect(500, 40);
+			
+			System.out.println(robot.readSelected());
+			
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
