e1d4815a7b987ff503ef333a7dbda471d55c1742
Matthew Underwood <matthew.b.underwood1995@gmail.com>
2014-07-30 08:46:37 +1000

    Added commented out server code for a pull later at work

--- src/com/github/RISKYScarecrow/ShopOrderHelper/ExtendedRobot.java
+++ src/com/github/RISKYScarecrow/ShopOrderHelper/ExtendedRobot.java
@@ -8,46 +8,87 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 
-
 public class ExtendedRobot extends Robot
 {
 
-	public ExtendedRobot() throws AWTException {
+	public ExtendedRobot() throws AWTException
+	{
 		super();
 	}
-	
-	public void keyUse(int kc){
-		keyPress(kc);
+
+	public void keyUse(int keyCode)
+	{
+		keyPress(keyCode);
 		delay(50);
-		keyRelease(kc);
+		keyRelease(keyCode);
 	}
-	
-	public void moveSelect(int _x, int _y){
+
+	public void moveSelect(int _x, int _y)
+	{
 		mouseMove(_x, _y);
-		mousePress(InputEvent.BUTTON1_MASK);
+		mousePress(InputEvent.BUTTON1_DOWN_MASK);
 		delay(10);
-		mouseRelease(InputEvent.BUTTON1_MASK);
-		
+		mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
 		keyPress(KeyEvent.VK_CONTROL);
 		keyUse(KeyEvent.VK_A);
 		keyRelease(KeyEvent.VK_CONTROL);
 	}
-	
-	public String readSelected(){
+
+	public void moveSelect2(int _x, int _y)
+	{
+		mouseMove(_x, _y);
+		delay(10);
+		mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		delay(10);
+		mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+
+
+		boolean on = false;
+		if (ShopOrderHelper.toolkit.getLockingKeyState(KeyEvent.VK_NUM_LOCK))
+		{
+			keyUse(KeyEvent.VK_NUM_LOCK);
+			on = true;
+		}
+
+		keyUse(KeyEvent.VK_HOME);
+		delay(10);
+		keyPress(KeyEvent.VK_SHIFT);
+		delay(10);
+		keyUse(KeyEvent.VK_END);
+		delay(10);
+		keyRelease(KeyEvent.VK_SHIFT);
+
+		if (on)
+		{
+			keyUse(KeyEvent.VK_NUM_LOCK);
+			on = false;
+		}
+
+	}
+
+	public String readSelected()
+	{
 		keyPress(KeyEvent.VK_CONTROL);
 		keyUse(KeyEvent.VK_C);
 		keyRelease(KeyEvent.VK_CONTROL);
-		
+
 		String result = null;
-		
-		try {
+
+		try
+		{
 			result = (String) ShopOrderHelper.clipboard.getData(DataFlavor.stringFlavor);
-		} catch (UnsupportedFlavorException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		catch (UnsupportedFlavorException e)
+		{
 			e.printStackTrace();
 		}
-		
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
 		return result;
 	}
 }
--- src/com/github/RISKYScarecrow/ShopOrderHelper/ShopOrderHelper.java
+++ src/com/github/RISKYScarecrow/ShopOrderHelper/ShopOrderHelper.java
@@ -22,18 +22,9 @@ public class ShopOrderHelper {
 			
 			
 			robot.delay(5000);
-			/*
-			robot.keyUse(KeyEvent.VK_H);
-			robot.keyUse(KeyEvent.VK_I);
-			robot.keyUse(KeyEvent.VK_SPACE);
-			robot.keyUse(KeyEvent.VK_B);
-			robot.keyUse(KeyEvent.VK_U);
-			robot.keyUse(KeyEvent.VK_D);
-			robot.keyUse(KeyEvent.VK_D);
-			robot.keyUse(KeyEvent.VK_Y);
-			*/
+
 			
-			robot.moveSelect(500, 40);
+			robot.moveSelect2(170, 40);
 			
 			System.out.println(robot.readSelected());
 			
@@ -43,3 +34,79 @@ public class ShopOrderHelper {
 	}
 
 }
+/* Server code for push and pull
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class SimpleServer extends Thread
+{
+	private ServerSocket serverSocket;
+
+	public SimpleServer(int port) throws IOException
+	{
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void run()
+	{
+		while (true)
+		{
+			try
+			{
+				System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+				System.out.println("Just connected to " + server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(server.getInputStream());
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(server.getOutputStream());
+				out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress());
+				for (int i = 0; i < 10; i++)
+				{
+					out.writeUTF("Iteration " + i);
+					try
+					{
+						Thread.sleep(100);
+					}
+					catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				out.writeUTF("Goodbye!");
+				out.writeUTF("Kill connection");
+				server.close();
+			}
+			catch (SocketTimeoutException s)
+			{
+				System.out.println("Socket timed out!");
+				break;
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+				break;
+			}
+		}
+		System.out.println("End!");
+	}
+
+	public static void main(String[] args)
+	{
+		int port = Integer.parseInt(args[0]);
+		try
+		{
+			Thread t = new SimpleServer(port);
+			t.start();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+*/
